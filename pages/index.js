63e053a6a1c7f219083999bcb23a168e0e87a2fd
Adam A Prasetya <adamworks23@gmail.com>
2024-05-12 09:03:34 +0700

    api function and get static props

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,12 @@ import ResourceHighlight from "@/components/ResourceHighlight"
 import Newsletter from "@/components/Newsletter"
 import ResourceList from "@/components/ResourceList"
 import Footer from "@/components/Footer"
-import { resources } from "@/api/data"
+import resources from "./api/resources"
+// import { resources } from "@/api/data"
 
 //export default function Home() {
-const Home = () => {
+const Home = ({resources}) => {
+  // debugger
   return (
     <Layout>
       <ResourceHighlight resources={resources.slice(0,2)} /> 
@@ -18,5 +20,32 @@ const Home = () => {
     </Layout>
   )
 }
+
+//server side props (called every time visit page)
+//funciton executed on the server
+export async function getServerSideProps() {
+  const resData = await fetch("http://localhost:3000/api/resources")
+  const data = await resData.json()
+
+  return {
+    props : {
+      resources : data
+    }
+  }
+}
+
+//static props (only called on build time - called only once)
+// export async function getStaticProps() {
+//   //resources static props - return promise
+//   const resData = await fetch("http://localhost:3000/api/resources")
+//   const data = await resData.json()
+//   return {
+//     props: {
+//       resources : data
+//       // myData1 :[1,2,3],
+//       // myData2 :["a", "b", "c"]
+//     }
+//   }
+// }
 
 export default Home
