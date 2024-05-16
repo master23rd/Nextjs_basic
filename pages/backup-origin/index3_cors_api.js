dd4faaa56ecbb1a9e52a2c610c96d31928244ccd
Adam A Prasetya <adamworks23@gmail.com>
2024-05-16 12:10:59 +0700

    add resources/new.js and Link next router

--- pages/backup-origin/index3_cors_api.js
+++ pages/backup-origin/index3_cors_api.js
@@ -4,14 +4,25 @@ import ResourceHighlight from "@/components/ResourceHighlight"
 import Newsletter from "@/components/Newsletter"
 import ResourceList from "@/components/ResourceList"
 import Footer from "@/components/Footer"
+// import { resources } from "@/api/data"
+import data from "./api/data.json" 
 
 //export default function Home() {
 const Home = ({resources}) => {
+
+  //running from client
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/api/resources") 
+  // }, [])
+
+  console.log('calling from home');
+  // debugger
   return (
     <Layout>
       <ResourceHighlight resources={resources.slice(0,2)} /> 
       <Newsletter />
       <ResourceList resources={resources.slice(2)}/>
+      {/* {JSON.stringify(resources)} */}
       <Footer />
     </Layout>
   )
@@ -29,5 +40,22 @@ export async function getServerSideProps() {
     }
   }
 }
+
+//static props (only called on build time - called only once)
+//function called on server
+// export async function getStaticProps() {
+//   //resources static props - return promise
+//   // const resData = await fetch("http://localhost:3000/api/resources")
+//   // const data = await resData.json()
+
+//   console.log('called staticprops');
+//   return {
+//     props: {
+//       resources : data
+//       // myData1 :[1,2,3],
+//       // myData2 :["a", "b", "c"]
+//     }
+//   }
+// }
 
 export default Home
---
+++ pages/resources/new.js
@@ -0,0 +1,13 @@
+import Layout from "@/components/Layout"
+
+const ResourceCreate = () => {
+    return (
+        <Layout>
+            <div>
+                Resource create page
+            </div>
+        </Layout>
+    )
+}
+
+export default ResourceCreate
